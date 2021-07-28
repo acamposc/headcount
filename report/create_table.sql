767e545ceedf01a0095df0c6cdb166372487d02e
arturo <arturplur@gmail.com>
2021-07-27 19:47:27 -0500

    identity

--- report/create_table.sql
+++ report/create_table.sql
@@ -3,11 +3,13 @@ set timezone = 'America/Lima';
 show timezone;
 
 create table won.clients(
-    id int primary key not null,
+    -- id int primary key not null,
+    id int not null generated always as identity (start with 1 increment by 1),
     client_name text,
     business_name text,
     country_code text,
-    tz text
+    tz text,
+    ingestion_time timestamp with time zone default current_timestamp
 
 );
 
--- report/insert_clients.sql
+++ report/insert_clients.sql
@@ -1,12 +1,12 @@
 --https://www.postgresqltutorial.com/import-csv-file-into-posgresql-table/
-COPY won.clients(id,client_name,business_name,country_code,tz)
+COPY won.clients(client_name,business_name,country_code,tz)
 FROM '/data/clients.csv'
 DELIMITER ','
 CSV HEADER;
 
 --https://stackoverflow.com/questions/34901804/how-work-with-unix-timestamp-on-postgresql
-alter table won.clients
-add column ts timestamp with time zone default current_timestamp;
+--alter table won.clients
+--add column ingestion_time timestamp with time zone default current_timestamp;
 
 --https://www.postgresqltutorial.com/postgresql-timestamp/
 --insert into clients(ts)
